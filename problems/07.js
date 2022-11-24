31e8ea8355de86426b3e31900f1529b2513b2f37
chan-kim <chan.ckeem@gmail.com>
2022-11-24 23:19:43 +0900

    solve 07

---
+++ problems/07.js
@@ -0,0 +1,24 @@
+const isPrime = N => {
+  // 1은 소수가 아니다.
+  if (N === 1) return false;
+  // 2부터 N-1까지의 수로 N을 나눴을 때
+  for (let i = 2; i <= N - 1; i++) {
+    // 나누어 떨어지는 경우가 한 번이라도 있으면 N은 소수가 아니다.
+    if (N % i === 0) return false;
+  }
+  // 모두 나누어 떨어지지 않으면 N은 소수이다.
+  return true;
+};
+
+let count = 1;
+let result = 0;
+
+for (i = 1; ; i++) {
+  if (isPrime(i)) {
+    if (count === 1_0001) {
+      console.log(i);
+      return;
+    }
+    count++;
+  }
+}
