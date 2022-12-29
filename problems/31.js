68aeb0e8ef335fde0c500a76819acdcba8eca4e2
chan-kim <chan.ckeem@gmail.com>
2022-12-30 00:33:33 +0900

    solve 32

---
+++ problems/31.js
@@ -0,0 +1,27 @@
+const pence = [1, 2, 5, 10, 20, 50, 100, 200];
+
+function slicePound(pound, i) {
+  console.log(pound, i);
+  if (i === 1) {
+    return Math.floor(pound / 2) + 1;
+  }
+
+  let cnt = 1;
+  let p = pound;
+  while (p > 0) {
+    cnt += slicePound(p, i - 1);
+    p -= pence[i];
+  }
+
+  if (p !== 0) {
+    cnt--;
+  }
+
+  return cnt;
+}
+
+function operate(pound) {
+  return slicePound(pound, pence.length - 1);
+}
+
+console.log(operate(200));
